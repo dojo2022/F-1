4c137e6eaac4c20019fc1619dfd8f924d6b7a1f2
dojo2022-hojo <hojo-yuki-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-08 14:50:27 +0900

    model

--- src/src/model/User.java
+++ src/src/model/User.java
@@ -3,5 +3,55 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable {
+	private int id;
+	private String user;
+	private String userId;
+	private String password;
+
+	public User(int id, String user, String userId, String password) {
+		this.id = id;
+		this.user = user;
+		this.userId = userId;
+		this.password = password;
+	}
+
+	public User() {
+		this.id = 0;
+		this.user = "";
+		this.userId = "";
+		this.password = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
