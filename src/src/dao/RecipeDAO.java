b7aff78d8fdc09e4eea43d46c8b1746f0ac89e7e
dojo2022-shimizu-natsuki <103239215+dojo2022-shimizu-natsuki@users.noreply.github.com>
2022-06-13 17:56:18 +0900

    06/13レシピ

--- src/src/dao/RecipeDAO.java
+++ src/src/dao/RecipeDAO.java
@@ -1,6 +1,128 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Recipe;
+
 public class RecipeDAO {
+    // 引数paramで検索項目を指定し、検索結果のリストを返す
+	public List<Recipe> select(Recipe param) {
+		Connection conn = null;
+		List<Recipe> recipeList = new ArrayList<Recipe>();
+
+        try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/GandA", "sa", "");
+
+			String sql;
+			PreparedStatement pStmt;
+			// SQL文を準備する
+			if(param.getTimeCooking() != 0){
+				sql = "SELECT name_dish, genre, ingredient, time_cooking, season, sub_recipe, image_recipe FROM RECIPE_SHOW WHERE name_dish LIKE ? AND genre LIKE ? AND ingredient LIKE ? AND time_cooking = ? ORDER BY name_dish";
+				pStmt = conn.prepareStatement(sql);
+			}else {
+				sql = "SELECT name_dish, genre, ingredient, time_cooking, season, sub_recipe, image_recipe FROM RECIPE_SHOW WHERE name_dish LIKE ? AND genre LIKE ? AND ingredient LIKE ? AND time_cooking >= 0 ORDER BY name_dish";
+				pStmt = conn.prepareStatement(sql);
+			}
+			// SQL文を完成させる
+			if (param.getTimeCooking() != 0) {
+				pStmt.setInt(4, param.getTimeCooking());
+			}
+
+			if (param.getNameDish() != null) {
+				pStmt.setString(1, "%" + param.getNameDish() + "%");
+			}
+			else {
+				pStmt.setString(1, "%");
+			}
+            if (param.getGenre() != null) {
+				pStmt.setString(2, "%" + param.getGenre() + "%");
+			}
+			else {
+				pStmt.setString(2, "%");
+			}
+            if (param.getIngredient() != null) {
+				pStmt.setString(3, "%" + param.getIngredient() + "%");
+			}
+			else {
+				pStmt.setString(3, "%");
+			}
+
+//			if (param.getNameDish() != null) {
+//				pStmt.setString(1, "%" + param.getNameDish() + "%");
+//			}
+//			else {
+//				pStmt.setString(1, "%");
+//			}
+//            if (param.getGenre() != null) {
+//				pStmt.setString(2, "%" + param.getGenre() + "%");
+//			}
+//			else {
+//				pStmt.setString(2, "%");
+//			}
+//            if (param.getIngredient() != null) {
+//				pStmt.setString(3, "%" + param.getIngredient() + "%");
+//			}
+//			else {
+//				pStmt.setString(3, "%");
+//			}
+//            if (param.getTimeCooking() != 0) {
+//				pStmt.setString(4, "%" + param.getTimeCooking() + "%");
+//			}
+//			else {
+//				pStmt.setString(4, "%");
+//			}
+
+            // SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Recipe recipe = new Recipe(
+				rs.getString("name_dish"),
+				rs.getString("genre"),
+				rs.getString("ingredient"),
+				rs.getInt("time_cooking"),
+				rs.getString("season"),
+				rs.getString("sub_recipe"),
+				rs.getString("image_recipe")
+				);
+				recipeList.add(recipe);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			recipeList = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			recipeList = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					recipeList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return recipeList;
+	}
 
 
 }
--- src/src/servlet/RecipeServlet.java
+++ src/src/servlet/RecipeServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.RecipeDAO;
+import model.Recipe;
 
 /**
  * Servlet implementation class RecipeServlet
@@ -18,16 +25,35 @@ public class RecipeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		/*HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/GandA/LoginServlet");
+			return;
+		}*/
+
+		// 検索処理を行う
+		RecipeDAO reDao = new RecipeDAO();
+		List<Recipe> recipeList = reDao.select(new Recipe("", "", "", 0, "", "", ""));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("recipeList", recipeList);
+
+		// レシピページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/recipe.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/GandA/LoginServlet");
+			return;
+		}
 	}
 
 }
