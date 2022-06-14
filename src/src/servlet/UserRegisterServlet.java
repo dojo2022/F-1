2f65c4abb5c8baab5a820436e143750fd0bfd04f
dojo2022-shinya <103238483+dojo2022-shinya@users.noreply.github.com>
2022-06-14 12:56:10 +0900

    Update UserRegisterServlet.java

--- src/src/servlet/UserRegisterServlet.java
+++ src/src/servlet/UserRegisterServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.UserRegisterDAO;
+import model.User;
 
 /**
  * Servlet implementation class UserRegisterServlet
@@ -26,8 +31,32 @@ public class UserRegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		/*HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/simpleBC/LoginServlet");
+			return;
+		}*/
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String user = request.getParameter("USER");
+		String userid = request.getParameter("USERID");
+		String password = request.getParameter("PASSWORD");
+
+
+		// 登録処理を行う
+		UserRegisterDAO udao = new UserRegisterDAO();
+		if (udao.insert(new User(0, user, userid,password))) {	// 登録成功
+			request.setAttribute("result","ユーザー登録が成功しました。");
+
+		}
+		else {												// 登録失敗
+			request.setAttribute("result","ユーザー登録が失敗しました。");
+		}
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_regist_result.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
