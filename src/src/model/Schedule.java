6a7095f7e22379c3314ed92e12f92299f80661c0
dojo-2022-sarita <sarita-takuya-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-15 15:19:40 +0900

    去田(schedule.java  修正版(新規))

--- src/src/model/Schedule.java
+++ src/src/model/Schedule.java
@@ -7,12 +7,16 @@ public class Schedule implements Serializable {
 	private String date;
 	private String sub;
 	private String tittle;
+	private String start_time;
+	private String end_time;
 
-	public Schedule(String user, String date, String sub,String tittle) {
+	public Schedule(String user, String date, String sub,String tittle,String start_time,String end_time) {
 		this.user = user;
 		this.date = date;
 		this.sub = sub;
 		this.tittle = tittle;
+		this.start_time = start_time;
+		this.end_time = end_time;
 	}
 
 	public Schedule() {
@@ -20,6 +24,8 @@ public class Schedule implements Serializable {
 		this.date = "";
 		this.sub = "";
 		this.tittle = "";
+		this.start_time = "";
+		this.end_time = "";
 	}
 
 	public String getUser() {
@@ -51,5 +57,19 @@ public class Schedule implements Serializable {
 
 	public void setTittle(String tittle) {
 		this.tittle = tittle;
+	}
+	public String getStart_time() {
+		return start_time;
+	}
+
+	public void setStart_time(String start_time) {
+		this.start_time = start_time;
+	}
+	public String getEnd_time() {
+		return end_time;
+	}
+
+	public void setEnd_time(String end_time) {
+		this.end_time = end_time;
 	}
 }
