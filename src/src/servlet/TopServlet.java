6291d68d16b0f4ee65ce7d238e8a093822d51f53
dojo-2022-sarita <sarita-takuya-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-20 11:19:24 +0900

    去田変更(TOP)

--- src/src/servlet/TopServlet.java
+++ src/src/servlet/TopServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.TopDAO;
+import model.Schedule;
 
 /**
  * Servlet implementation class TopServlet
@@ -20,16 +25,60 @@ public class TopServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//HttpSession session = request.getSession();
+//	if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/simpleBC/LoginServlet");
+//			return;
+//		}
+
+//		HttpSession session = request.getSession();
+//	String user_id = (session.getAttribute("user_id")).toString();
+
+//
+
+		// 検索処理を行う
+		TopDAO toDao = new TopDAO();
+		List<Schedule> todoList = toDao.selectTodo(new Schedule("a","","","","",""));
+		//List<Schedule> cardList = toDao.selectTodo(new Schedule(user,date,sub,title,start_time,end_time));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("todoList", todoList);
+
+		// 登録ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp?str=1");
 		dispatcher.forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+//	if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/simpleBC/LoginServlet");
+//			return;
+//		}
 
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String user = "a";
+				String date = request.getParameter("DATE");
+				String sub = request.getParameter("SUB");
+				String title = request.getParameter("TITLE");
+				String start_time = request.getParameter("START_TIME");
+				String end_time = request.getParameter("END_TIME");
+
+				String[] dateNow = new String[3];
+				dateNow[0] = request.getParameter("year");
+				dateNow[1] = request.getParameter("month");
+				dateNow[2] = request.getParameter("day");
+
+				// 検索結果をリクエストスコープに格納する
+				session.setAttribute("dateNow", dateNow);
+
+				// 登録処理を行う
+				TopDAO toDao = new TopDAO();
+				boolean todoList = toDao.insert(new Schedule("a","","","","",""));
+
+				//登録サーブレットにリダイレクトする
+				response.sendRedirect("/GandA/TopServlet");
+	}
 }
